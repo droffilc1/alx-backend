fb9f016aec6da9b4e76374fa551c1c9ee0611a0c
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-05-17 11:42:46 +0300

    feat: simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" 0-simple_helper_function """
+
+
+def index_range(page, page_size):
+    """Simple helper function.
+
+    Args:
+        page: pages
+        page_size: page size
+
+    Returns a tuple of size two containing a start index and an end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)

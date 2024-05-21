b97def0391846d4745c49e2741a077980f384cec
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-05-21 19:47:57 +0300

    fix: basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,16 +7,22 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """A class BasicCache that inherits from BaseCaching."""
+    """BasicCache is a caching system that inherits from BaseCaching.
+    This caching system doesn't have a limit on the number of items stored.
+    """
 
     def put(self, key, item):
-        """Add an item to the cache."""
-        if key is None or item is None:
-            pass
-        self.cache_data[key] = item
+        """Add an item to the cache.
+
+        If key or item is None, this method does nothing.
+        """
+        if key is not None or item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """Get an item by key."""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data.get(key)
+        """Get an item by key.
+
+        If key is None or if the key doesn't exist in self.cache_data,
+        return None.
+        """
+        return self.cache_data.get(key, None)

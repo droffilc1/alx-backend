6fe0a9e62ee78cc750f8f4df6378e5fcc2e0dbbf
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-05-21 19:50:25 +0300

    fix: FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -17,9 +17,8 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """Add an item in the cache."""
-        if key or item is None:
-            pass
-        self.cache_data[key] = item
+        if key is not None or item is not None:
+            self.cache_data[key] = item
         if len(self.cache_data.items()) > BaseCaching.MAX_ITEMS:
             # discards the first item put in cache(FIFO algorithm)
             first_key = next(iter(self.cache_data))
@@ -28,6 +27,4 @@ class FIFOCache(BaseCaching):
 
     def get(self, key):
         """Get an item by key."""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)

b72384a7aaf5d660ac3f418ce509839a090d5c6c
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-06-27 12:28:03 +0300

    feat: writing the job creation function

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,26 @@
+// Writing the job creation function
+const createPushNotificationsJobs = (jobs, queue) => {
+  if (!Array.from(jobs)) {
+    throw Error('Jobs is not an array');
+  }
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code_3', jobData)
+      .save((err) => {
+        if (!err) console.log(`Notification job created: ${job.id}`);
+      });
+
+    job.on('complete', () => {
+      console.log(`Notification job #${job.id} completed`);
+    });
+
+    job.on('failure', (err) => {
+      console.log(`Notification job #${job.id} failed: ${err.message}`);
+    });
+
+    job.on('progress', (progress) => {
+      console.log(`Notification job #${job.id} ${progress}% complete`);
+    });
+  });
+}
+
+module.exports = createPushNotificationsJobs;
